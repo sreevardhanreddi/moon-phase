681494aa7134a5954749824b5a954ac811c838c6
sreevardhanreddi <sreevardhanreddi@poshn.co>
2025-12-15 11:36:03 +0530

    chore: update build process to include production environment for CSS optimization with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(isProd ? { cssnano: {} } : {}),
   },
-}
+};
 
